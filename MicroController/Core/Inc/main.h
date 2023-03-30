37c72a07058f69cea5de7c54ad0b10dc6b774b86
izeek182 <izeek182@gmail.com>
2023-03-29 22:28:03 -0600

    added All the need pins

--- MicroController/Core/Inc/main.h
+++ MicroController/Core/Inc/main.h
@@ -57,6 +57,34 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define TriggerA_Pin GPIO_PIN_1
+#define TriggerA_GPIO_Port GPIOC
+#define TriggerB_Pin GPIO_PIN_2
+#define TriggerB_GPIO_Port GPIOC
+#define Motor1En_Pin GPIO_PIN_0
+#define Motor1En_GPIO_Port GPIOA
+#define Motor1A_Pin GPIO_PIN_1
+#define Motor1A_GPIO_Port GPIOA
+#define Motor1B_Pin GPIO_PIN_2
+#define Motor1B_GPIO_Port GPIOA
+#define Motor2En_Pin GPIO_PIN_6
+#define Motor2En_GPIO_Port GPIOA
+#define Motor2A_Pin GPIO_PIN_7
+#define Motor2A_GPIO_Port GPIOA
+#define Motor2B_Pin GPIO_PIN_4
+#define Motor2B_GPIO_Port GPIOC
+#define EchoB_Pin GPIO_PIN_14
+#define EchoB_GPIO_Port GPIOB
+#define EchoA_Pin GPIO_PIN_15
+#define EchoA_GPIO_Port GPIOB
+#define rLED_Pin GPIO_PIN_6
+#define rLED_GPIO_Port GPIOC
+#define bLED_Pin GPIO_PIN_7
+#define bLED_GPIO_Port GPIOC
+#define oLED_Pin GPIO_PIN_8
+#define oLED_GPIO_Port GPIOC
+#define gLED_Pin GPIO_PIN_9
+#define gLED_GPIO_Port GPIOC
 
 /* USER CODE BEGIN Private defines */
 
